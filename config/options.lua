cbf2e9c8d31ece8d2b72ad72ae77e03eedd515cb
Gustavo Mendes <gustavo.mendes@picpay.com>
2025-07-11 09:14:52 -0300

    feat: adicionando tab com 4 espacos

---
+++ config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.tabstop = 4 -- Quantidade de espaços que um <Tab> representa
+vim.opt.shiftwidth = 4 -- Quantidade de espaços usados para indentação automática
+vim.opt.softtabstop = 4 -- Quantidade de espaços ao usar <Tab> em insert mode
+vim.opt.expandtab = true -- Converte <Tab> em espaços
